f499f97d88bbf3cb0d848325c22807cc4d2439e1
Babacar-seck <babacarseckdevweb@gmail.com>
2020-12-22 14:25:41 +0100

    front

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+{/* The following line can be included in your src/index.js or App.js file*/ }
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
